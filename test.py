c5c3b330911074f94304d3bba01e58370762583e
jeromeCompain <compain10@hotmail.com>
2017-01-18 22:11:36 +0100

    démo doudou
    on teste la boucle

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 first_words = "hello world!"
 print(first_words)
+Rosalind = "http://rosalind.info/problems/list-view/"
+personne = "mon doudou"
+software = "Eclipse"
+for x in range(1, 11):
+    print('je montre a {0} pour la {2} fois comment marche {1}'.format(personne, software, x))
